86229475d543d99a2ccffb0b04688a1da0a70bb2
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-09-05 15:28:07 +0530

    feat(server): add middleware and frontend URL config
    - Add cookie-parser, morgan, and cors middleware to express app
    - Include FRONTEND_URL in config for future CORS origin configuration
    - Update package.json with new dependencies and their types

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,23 @@
 import express from "express";
 import indexRoute from "./routes/index.route.ts";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    // origin: config.FRONTEND_URL,
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/", indexRoute);
 
--- server/src/configs/config.ts
+++ server/src/configs/config.ts
@@ -8,6 +8,7 @@ const _config = {
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
 
 const config = Object.freeze(_config);
