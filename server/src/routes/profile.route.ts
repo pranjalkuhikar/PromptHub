4775bc06daaff4720c34614ceff03e2d88b1518a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-09-05 15:57:57 +0530

    refactor(routes): remove unused authenticate middleware from profile route

--- server/src/routes/profile.route.ts
+++ server/src/routes/profile.route.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { profile } from "../controllers/user.controller.ts";
-import { authenticate } from "../middlewares/auth.middleware.ts";
 
 const profileRoute = express.Router();
 
